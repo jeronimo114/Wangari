57e1e9c728be64119c8bd8c500a274c55f67437a
Jeronimo114 <jeronimomartinezjaramillo@gmail.com>
2025-04-22 13:55:45 -0500

    Hero modification 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,6 +112,17 @@
   // ==================================================
   const ScrollAnimations = (() => {
     const initAnimations = () => {
+      // Parallax vertical para la imagen de fondo del hero
+      gsap.to(".hero-image", {
+        backgroundPositionY: "100%",
+        ease: "none",
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      });
       // Efecto Parallax para la sección hero:
       gsap.to(".hero", {
         backgroundPosition: "50% 100%",
